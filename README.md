f35792063d1673fb1df0afe697608618a2a6a8d9
kerubkim <kerubkimq@gmail.com>
2019-08-22 14:00:50 -0700

    CLEANUP
    • added a file into gitignore for routes
    • new README.md

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ This boiler plate is for client-side reactjs server side rendering with google a
 
 Instructions:
 
-1. npm install
-2. npm run dev
+1. cd server
+2. npm install
+3. cd server/src/static/strings
+4. touch rootEndpoint.js
+    - add necessary root endpoints
+5. cd server
+6. npm run dev
 
 
 Course: https://www.udemy.com/server-side-rendering-with-react-and-redux/
--- server/src/static/strings/apiEndpoints.js
+++ server/src/static/strings/apiEndpoints.js
@@ -1,5 +1,7 @@
-export const SERVER_SIDE_BASE_URL = 'http://react-ssr-api.herokuapp.com';
-export const CLIENT_SIDE_BASE_URL = '/api';
+import {ROOT_URL_SERVER_SIDE, ROOT_URL_CLIENT_SIDE} from "./rootEndpoint";
+
+export const SERVER_SIDE_BASE_URL = ROOT_URL_SERVER_SIDE;
+export const CLIENT_SIDE_BASE_URL = ROOT_URL_CLIENT_SIDE;
 
 export const FETCH_USERS = '/users';
 export const FETCH_CURRENT_USER = '/current_user';
